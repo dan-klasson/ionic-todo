cc0ed34814979c8be9ef0a80764640f880f50899
Daniel Klasson <dan.klasson@gmail.com>
2016-08-07 14:52:18 +0700

    removed default code

--- www/js/app.js
+++ www/js/app.js
@@ -40,16 +40,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
   .state('tab.todos', {
     url: '/todos',
     views: {
@@ -80,25 +70,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   })
 
-  .state('tab.chats', {
-    url: '/chats',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/tab-chats.html',
-        controller: 'ChatsCtrl'
-      }
-    }
-  })
-  .state('tab.chat-detail', {
-    url: '/chats/:chatId',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/chat-detail.html',
-        controller: 'ChatDetailCtrl'
-      }
-    }
-  })
-
   .state('tab.account', {
     url: '/account',
     views: {
@@ -110,6 +81,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/todos');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,5 @@
 angular.module('starter.controllers', ['ngStorage'])
 
-.controller('DashCtrl', function($scope) {})
-
 .controller('TodosCtrl', function($scope, Todos) {
   $scope.todos = Todos.all();
   $scope.remove = function(todo) {
@@ -27,29 +25,6 @@ angular.module('starter.controllers', ['ngStorage'])
       $state.go('tab.todos');
     }
   };
-})
-
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  console.log(Chats.all())
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
